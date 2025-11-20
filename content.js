09f7f68399b0f860dffcd15a388aa0dde36b34fc
Matthew Adcock <pilotmaddmatt@gmail.com>
2025-11-20 13:32:26 -0700

    adds button to login page for testing

--- content.js
+++ content.js
@@ -1,24 +1,19 @@
-function renderReadingTime(h1) {
-  // If we weren't provided an h1, we don't need to render anything.
-  if (!h1) {
+function renderReadingTime(loginPanel) {
+  // If we weren't provided a main, we don't need to render anything.
+  if (!loginPanel) {
     return;
   }
 
   const badge = document.createElement("button");
   // TODO: Add button styles similar to others on page
-  badge.classList.add("color-secondary-text", "type--caption");
+  badge.classList.add("btn", "btn-dark");
 
   badge.textContent = `Download`;
 
   // Support for API reference docs
-  const downloadButton = h1.querySelector("h1");
+  const downloadButton = loginPanel.querySelector(".col-sm-7");
 
-  downloadButton.insertAdjacentElement("afterend", badge);
+  downloadButton.insertAdjacentElement("afterbegin", badge);
 }
 
-renderReadingTime(document.querySelector("h1"));
-
-// script won't be reinjected when this happens, so we need to watch for changes to the content.
-observer.observe(document.querySelector("devsite-content"), {
-  childList: true,
-});
+renderReadingTime(document.querySelector(".login-panel"));
