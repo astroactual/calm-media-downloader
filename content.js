55a298076f71d4aa39d0bee08725c02bcc975908
Matthew Adcock <pilotmaddmatt@gmail.com>
2025-11-20 13:18:15 -0700

    changed from popup to content in js

--- content.js
+++ content.js
@@ -1,14 +1,24 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  const movieTitle = "ng-binding ng-scope";
-
-  //const mediaTitle = bindingTitle ?? scopeTitle;
-
-  if (request.action === "getSpanText") {
-    const spanElement = document.querySelector(`span.${movieTitle}`); // Target the first span element
-    if (spanElement) {
-      sendResponse({ spanText: spanElement.textContent });
-    } else {
-      sendResponse({ spanText: null });
-    }
+function renderReadingTime(h1) {
+  // If we weren't provided an h1, we don't need to render anything.
+  if (!h1) {
+    return;
   }
+
+  const badge = document.createElement("button");
+  // TODO: Add button styles similar to others on page
+  badge.classList.add("color-secondary-text", "type--caption");
+
+  badge.textContent = `Download`;
+
+  // Support for API reference docs
+  const downloadButton = h1.querySelector("h1");
+
+  downloadButton.insertAdjacentElement("afterend", badge);
+}
+
+renderReadingTime(document.querySelector("h1"));
+
+// script won't be reinjected when this happens, so we need to watch for changes to the content.
+observer.observe(document.querySelector("devsite-content"), {
+  childList: true,
 });
