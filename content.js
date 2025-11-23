f2518aa8b5677c1cc5ebf78f242454919ea3cdc2
astroactual <pilotmaddmatt@gmail.com>
2025-11-23 16:32:13 -0700

    added alert if no video is available to download

--- content.js
+++ content.js
@@ -6,32 +6,34 @@ function renderButton(masterSection) {
 
   //create button w/ styles
   const button = document.createElement("button");
-  button.style.marginLeft = "1em";
+  button.style.marginTop = "-4px";
   button.style.borderRadius = "6px";
-  button.style.height = "32px";
-  button.style.lineHeight = "32px";
+  button.style.height = "30px";
+  button.style.lineHeight = "30px";
+
   //Button styles similar to others on page
   button.classList.add("btn", "btn-trans-brand", "download-button");
   //content of button
   button.textContent = `Download`;
 
   //Select button location
-  const buttonLocation = masterSection.querySelector(".ff-icon-movie-about");
+  const buttonLocation = masterSection.querySelector(".movie-menu-icons");
   //insert button
-  buttonLocation.insertAdjacentElement("afterend", button);
+  buttonLocation.insertAdjacentElement("afterbegin", button);
 
   //wait for click
-  button.addEventListener('click', function() {
-        // find video element on page when clicked, open src in new tab to download
-        const videoElement = document.querySelector("video");
-        if (!videoElement)  {
-          return;
-        }
-        //post src in console, just cause why not
-        console.log('Video Source:', videoElement.src);
-        //open link in new blank tab
-        window.open(videoElement.src, '_blank');
-    });
+  button.addEventListener("click", function () {
+    // find video element on page when clicked, open src in new tab to download
+    const videoElement = document.querySelector("video");
+    if (!videoElement) {
+      window.alert('Please open a video player before clicking "Download"');
+      return;
+    }
+    //post src in console, just cause why not
+    console.log("Video Source:", videoElement.src);
+    //open link in new blank tab
+    window.open(videoElement.src, "_blank");
+  });
 }
 
 //render button!
