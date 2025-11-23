a2f7a0f2d269434a6590c23066ccb7858a1cae56
astroactual <pilotmaddmatt@gmail.com>
2025-11-23 00:53:27 -0700

    added event listener to allow downloads, and added styles

--- content.js
+++ content.js
@@ -1,11 +1,15 @@
 function renderButton(masterSection) {
-  // If we weren't provided .movie-menu, we don't need to render anything.
+  // if not provided a .movie-menu, render nothing
   if (!masterSection) {
     return;
   }
 
-  //create button
+  //create button w/ styles
   const button = document.createElement("button");
+  button.style.marginLeft = "1em";
+  button.style.borderRadius = "6px";
+  button.style.height = "32px";
+  button.style.lineHeight = "32px";
   //Button styles similar to others on page
   button.classList.add("btn", "btn-trans-brand", "download-button");
   //content of button
@@ -16,10 +20,19 @@ function renderButton(masterSection) {
   //insert button
   buttonLocation.insertAdjacentElement("afterend", button);
 
-  //Event listener goes here
+  //wait for click
+  button.addEventListener('click', function() {
+        // find video element on page when clicked, open src in new tab to download
+        const videoElement = document.querySelector("video");
+        if (!videoElement)  {
+          return;
+        }
+        //post src in console, just cause why not
+        console.log('Video Source:', videoElement.src);
+        //open link in new blank tab
+        window.open(videoElement.src, '_blank');
+    });
 }
 
-//render button when movie menu is present
-//TODO: render button only when video link is present??
-//TODO or add event listener to button, when clicked, find video element and load src in new tab
+//render button!
 renderButton(document.querySelector(".movie-menu"));
